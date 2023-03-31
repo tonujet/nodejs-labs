3b97dcf4577d8baadf1e0c7400743f5cef1a3e7e
Antonio <tonujet@gmail.com>
2023-03-31 02:02:17 +0200

    Add solution to task 2

---
+++ Lab2/2-task.js
@@ -0,0 +1,19 @@
+// Задача 2. Напишіть функцію, яка бере два рядки і повертає true, якщо вони є анаграмами одне одного.
+const SEPARATOR = '';
+const isAnagrams = (a, b) => {
+    const charsA = a.toLowerCase().split(SEPARATOR);
+    const charsB = b.toLowerCase().split(SEPARATOR);
+    for (const charA of charsA) {
+        const index = charsB.indexOf(charA);
+        if (index >= 0) {
+            delete charsB[index];
+        } else return false;
+    }
+    const emptyArr = charsB.filter(el => el);
+    return emptyArr.length <= 0;
+};
+
+console.log(isAnagrams('listen', 'silten'));
+console.log(isAnagrams('listen', 'siltten'));
+console.log(isAnagrams('listen', 'netsil'));
+console.log(isAnagrams('listenn', 'nsilten'));
