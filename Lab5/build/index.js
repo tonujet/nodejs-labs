460bf1eb23cd297d208b05b060bf12a211e35e6f
Antonio <tonujet@gmail.com>
2023-05-05 18:30:11 +0200

    Initial server config

--- Lab5/build/index.js
+++ Lab5/build/index.js
@@ -1,1 +1,16 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+app.listen(PORT, () => {
+    console.log("Server was started");
+});
---
+++ Lab5/src/index.ts
@@ -0,0 +1,15 @@
+import express, {Request, Response, Express} from "express"
+import {config} from "dotenv"
+config()
+
+const app: Express = express()
+const PORT = process.env.PORT || 5000
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(PORT, () => {
+    console.log("Server was started");
+})
