59cc9c9a3acaf78aadeb49fcca50bc3cfae7886b
Antonio <tonujet@gmail.com>
2023-05-08 23:33:46 +0200

    Add additional dependencies

--- Lab5/build/index.js
+++ Lab5/build/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const env_enum_1 = require("./common/enums/env/env.enum");
 const routes_1 = require("./routes/routes");
 const api_routes_enum_1 = require("./common/enums/api/api-routes.enum");
+require("reflect-metadata");
 const app = (0, express_1.default)();
 const PORT = env_enum_1.ENV.APP.PORT;
 app.use(express_1.default.json());
--- Lab5/src/index.ts
+++ Lab5/src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response, Express} from "express"
 import {ENV} from "./common/enums/env/env.enum";
 import {apiRouter} from "./routes/routes"
 import {ApiRoutes} from "./common/enums/api/api-routes.enum";
+import 'reflect-metadata';
 
 const app: Express = express()
 const PORT = ENV.APP.PORT
