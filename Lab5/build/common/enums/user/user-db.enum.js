40d70e3d43862a5d6efef5d38d87118e930a5574
Antonio <tonujet@gmail.com>
2023-05-08 23:36:23 +0200

    Create db in file system

---
+++ Lab5/build/common/enums/user/user-db.enum.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserDbEnum = void 0;
+var UserDbEnum;
+(function (UserDbEnum) {
+    UserDbEnum["storage"] = "D:\\io\\University\\Semester 4\\nodejs\\nodejs-labs\\Lab5\\src\\db\\storage\\user\\user-storage.csv";
+    UserDbEnum["options"] = "D:\\io\\University\\Semester 4\\nodejs\\nodejs-labs\\Lab5\\src\\db\\options\\user\\user-options";
+})(UserDbEnum || (UserDbEnum = {}));
+exports.UserDbEnum = UserDbEnum;
---
+++ Lab5/src/common/enums/user/user-db.enum.ts
@@ -0,0 +1,6 @@
+enum UserDbEnum {
+    storage = "D:\\io\\University\\Semester 4\\nodejs\\nodejs-labs\\Lab5\\src\\db\\storage\\user\\user-storage.csv",
+    options = "D:\\io\\University\\Semester 4\\nodejs\\nodejs-labs\\Lab5\\src\\db\\options\\user\\user-options",
+}
+
+export {UserDbEnum}
