c10ea5293b9a9bbf1cafe5221023e0887ba95dd5
Antonio <tonujet@gmail.com>
2023-05-18 17:21:06 +0200

    Add enums

---
+++ Lab6/src/common/enum/db/tablenames.enum.ts
@@ -0,0 +1,9 @@
+export enum TablenamesEnum {
+    USER = "users",
+    CHANNEL = "channels",
+    VIDEO = "videos",
+    SUBSCRIPTION = "subscriptions",
+    COMMENT = "comments",
+    LIKE = "likes"
+}
+
---
+++ Lab6/src/common/enum/subcription/subscription-level.enum.ts
@@ -0,0 +1,6 @@
+export enum SubscriptionLevelEnum {
+   STANDART = "standart",
+   FAN = "fan",
+   FOLLOWER = "follower",
+   VIP = "vip"
+}
