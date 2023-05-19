316f1625495b9dfc4febe233291885741dde1e84
Antonio <tonujet@gmail.com>
2023-05-19 16:22:47 +0200

    typeorm config

---
+++ Lab7/src/index.ts
@@ -0,0 +1,11 @@
+import "reflect-metadata"
+import {dataSource} from "./typeorm.config.js";
+
+
+dataSource
+    .initialize()
+    .then(() => console.log("Connected to db"))
+    .catch(err => console.log(err));
+
+// console.log(process.env.HOST);
+
