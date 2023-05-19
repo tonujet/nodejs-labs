446e72af3abdfaa26be1f6a6304de3473eb33291
Antonio <tonujet@gmail.com>
2023-05-19 16:59:54 +0200

    Add user and post entity

---
+++ Lab7/src/db/entities/post.entity.ts
@@ -0,0 +1,25 @@
+import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn} from "typeorm";
+import {UserEntity} from "./user.entity.js";
+// id: uuid or number
+// dateCreation: Date required
+// title: string required
+// text: string required
+// userId: uuid or number
+
+@Entity({name: "posts"})
+export class PostEntity {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    title: string;
+
+    @Column()
+    text: string;
+
+    @ManyToOne(() => UserEntity, user => user.posts)
+    user: UserEntity;
+
+    @CreateDateColumn({name: 'created_at'})
+    dateCreation: Date
+}
---
+++ Lab7/src/db/entities/user.entity.ts
@@ -0,0 +1,39 @@
+// id: number or uuid
+// username: string required unique
+// email: string required
+// age: number required
+// info: string not required
+// adress: json { city: string, street: string}
+
+import {Column, Entity, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import {PostEntity} from "./post.entity.js";
+
+
+@Entity({name: "users"})
+export class UserEntity {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({unique: true})
+    username: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    age: number;
+
+    @Column({nullable: true})
+    info: string;
+
+    @Column({type: "simple-json", nullable: true})
+    address: {
+        city: string,
+        street: string,
+    };
+
+    @OneToMany(() => PostEntity, post => post.user)
+    posts: PostEntity[]
+
+}
