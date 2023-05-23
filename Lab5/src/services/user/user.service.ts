a732972c2182e3898be4a19ef7efefeb6bcaf4a8
Antonio <tonujet@gmail.com>
2023-05-23 18:37:50 +0200

    Refactor async functions

--- Lab5/src/services/user/user.service.ts
+++ Lab5/src/services/user/user.service.ts
@@ -1,7 +1,5 @@
 import {UserRepository} from "../../repositories/repositories.js";
-import {UserEntity} from "../../entity/user/user.entity.js";
-import {UserProperties} from "../../common/types/user/user.properties.js";
-
+import {CreateUserDto} from "../../dtos/user/create-user-dto.js";
 
 
 class UserService {
@@ -9,24 +7,24 @@ class UserService {
         private readonly userRepo: UserRepository
     ) {}
 
-    async get(id: number) {
-        return await this.userRepo.get(id);
+    get(id: number) {
+        return this.userRepo.get(id);
     };
 
-    async getAll() {
-        return await this.userRepo.getAll();
+    getAll() {
+        return this.userRepo.getAll();
     };
 
-    async create(user: UserEntity) {
-        return await this.userRepo.create(user);
+    create(user: CreateUserDto) {
+        return this.userRepo.create(user);
     };
 
-    async delete(id: number) {
-        return await this.userRepo.delete(id);
+    delete(id: number) {
+        return this.userRepo.delete(id);
     };
 
-    async update(properties: UserProperties, id: number) {
-        return await this.userRepo.update(properties, id);
+    update(userProps: CreateUserDto, id: number) {
+        return this.userRepo.update(userProps, id);
     };
 
 }
