57ccb624036124ae811173999a0238f02cb70c64
Antonio <tonujet@gmail.com>
2023-05-23 23:56:44 +0200

    Fix update user bugs

--- Lab5/src/repositories/user/user.repository.ts
+++ Lab5/src/repositories/user/user.repository.ts
@@ -1,9 +1,9 @@
 import fsp from "fs/promises";
-import {UserEntity} from "@entity/user/user.entity.js";
-import {CsvParser} from "@db/parsers/csvParser.js";
-import {OptionParser} from "@db/parsers/optionParser.js";
-import {UserValidator} from "@entity/user/user-validator.js";
-import {CreateUserDto} from "@dtos/user/create-user-dto.js";
+import { UserEntity } from "@entity/user/user.entity.js";
+import { CsvParser } from "@db/parsers/csvParser.js";
+import { OptionParser } from "@db/parsers/optionParser.js";
+import { UserValidator } from "@entity/user/user-validator.js";
+import { CreateUserDto } from "@dtos/user/create-user-dto.js";
 
 class UserRepository {
     private currId: number;
@@ -20,28 +20,28 @@ class UserRepository {
         this.header = keys.join(",");
     }
 
-
     async get(id: number): Promise<UserEntity> {
         const users = await this.getAll();
         const receivedUser = users.find(user => user.id === id);
         this.userValidator.isExist(receivedUser, id);
         return receivedUser!;
-    };
+    }
 
     getAll(): Promise<UserEntity[]> {
         return fsp
-            .readFile(this.storagePath, {encoding: "utf-8"})
-            .then(users => this.csvParser.parseEntities(this.header, users, this.keys));
-    };
+            .readFile(this.storagePath, { encoding: "utf-8" })
+            .then(users =>
+                this.csvParser.parseEntities(this.header, users, this.keys)
+            );
+    }
 
     create(user: CreateUserDto): Promise<UserEntity> {
-        const createdUser = {id: this.currId++, ...user};
+        const createdUser = { id: this.currId++, ...user };
         const csvLine = this.csvParser.entityToCsvRaw(createdUser);
         return fsp
-            .appendFile(this.storagePath, `\n${csvLine}`, {encoding: "utf-8"})
+            .appendFile(this.storagePath, `\n${csvLine}`, { encoding: "utf-8" })
             .then(() => createdUser);
-    };
-
+    }
 
     async delete(id: number): Promise<UserEntity> {
         const deletedUser = await this.get(id);
@@ -49,17 +49,20 @@ class UserRepository {
         const updatedUsers = users.filter(user => user.id !== id);
         await this.save(updatedUsers);
         return deletedUser;
-    };
-
+    }
 
     async update(userProps: CreateUserDto, id: number): Promise<UserEntity> {
         const users = await this.getAll();
         let updatedUser = null;
         const updatedUsers = users.map(user => {
             if (user.id === id) {
-                for(const key in (userProps as Record<keyof CreateUserDto, keyof typeof CreateUserDto>)) {
+                for (const key in userProps as Record<
+                    keyof CreateUserDto,
+                    keyof typeof CreateUserDto
+                >) {
                     const typedKey = key as keyof CreateUserDto;
-                    user[typedKey] = userProps[typedKey];
+                    const propValue =  userProps[typedKey];
+                    if(propValue) user[typedKey] = propValue;
                 }
                 updatedUser = user;
             }
@@ -68,26 +71,28 @@ class UserRepository {
         const user = this.userValidator.isExist(updatedUser, id);
         await this.save(updatedUsers);
         return user;
-    };
+    }
 
     private save(users: UserEntity[]): Promise<void> {
-        let csvRaws= this.csvParser.arrayToCsvRaws(users);
+        let csvRaws = this.csvParser.arrayToCsvRaws(users);
         if (csvRaws) csvRaws = `${this.header}\n${csvRaws}`;
         else csvRaws = `${this.header}`;
-        return fsp.writeFile(this.storagePath, csvRaws, {encoding: "utf-8"});
+        return fsp.writeFile(this.storagePath, csvRaws, { encoding: "utf-8" });
     }
 
     logOptions(): Promise<void> {
-        const options = {currId: String(this.currId)};
+        const options = { currId: String(this.currId) };
         const rawOptions = this.optionParser.convertToOptions(options);
         return fsp.writeFile(this.optionsPath, rawOptions);
     }
 
     async readOptions(): Promise<void> {
-        const rawOptions = await fsp.readFile(this.optionsPath, {encoding: "utf-8"});
+        const rawOptions = await fsp.readFile(this.optionsPath, {
+            encoding: "utf-8",
+        });
         const options = this.optionParser.parseOptions(rawOptions);
         this.currId = Number(options.currId);
     }
 }
 
-export {UserRepository};
+export { UserRepository };
