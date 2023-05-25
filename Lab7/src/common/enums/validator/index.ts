c68e45bb9473f954716a12933fe1dcbeeaec28c1
Antonio <tonujet@gmail.com>
2023-05-25 14:12:31 +0200

    Relocate validation options to separate folder

---
+++ Lab7/src/common/enums/validator/index.ts
@@ -0,0 +1,4 @@
+import { createValidationOptions } from "@enum/validator/create-validation-options.js";
+import { updateValidationOptions } from "@enum/validator/update-validation-options.js";
+
+export {createValidationOptions, updateValidationOptions}
